c93f6e49e375452ac7f063aee1090424a4f31af7
FrostedOrchard <58378766+FrostedOrchard@users.noreply.github.com>
2020-09-22 00:59:54 -0400

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,16 @@
+u// Simple C++ program to display "Hello World" 
+//Author: Miguel Manzanarez <gi3650@wayne.edu>
+// Header file for input output functions 
+#include<iostream>  
+  
+using namespace std; 
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Changing the Greetings"; 
+      
+    return 0; 
+} 
